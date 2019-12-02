16a9df588d3f57788f55f87ade02750141fae11b
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2019-12-03 00:00:01 +0100

    Finish tests.

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,8 @@
-use crate::lattice::reification::{Generalizable, ReificationError, TryReifiable};
 use crate::lattice::{TypeChecker, UpperBounded};
+use crate::{Generalizable, ReificationError, TryReifiable, TypeCheckKey};
 use ena::unify::{UnifyKey, UnifyValue};
 use std::cmp::max;
+use std::convert::TryInto;
 
 #[derive(Debug, Clone, Copy, PartialOrd, PartialEq, Ord, Eq, Hash)]
 struct Key(u32);
@@ -11,12 +12,16 @@ enum AbstractType {
     Any,
     Fixed(u8, u8),
     Integer(u8),
+    Numeric,
+    Bool,
     Error,
 }
 
+#[derive(Debug, Clone, Copy, PartialOrd, PartialEq, Ord, Eq, Hash)]
 enum ConcreteType {
     Int128,
     FixedPointI64F64,
+    Bool,
 }
 
 impl TryReifiable for AbstractType {
@@ -30,6 +35,8 @@ impl TryReifiable for AbstractType {
             AbstractType::Integer(_) => Err(ReificationError::Conflicting),
             AbstractType::Fixed(i, f) if *i <= 64 && *f <= 64 => Ok(ConcreteType::FixedPointI64F64),
             AbstractType::Fixed(_, _) => Err(ReificationError::Conflicting),
+            AbstractType::Numeric => Err(ReificationError::TooGeneral), // Note: it would also make sense e.g. to default to an integer here.
+            AbstractType::Bool => Ok(ConcreteType::Bool),
         }
     }
 }
@@ -41,6 +48,7 @@ impl Generalizable for ConcreteType {
         match self {
             ConcreteType::FixedPointI64F64 => AbstractType::Fixed(64, 64),
             ConcreteType::Int128 => AbstractType::Integer(128),
+            ConcreteType::Bool => AbstractType::Bool,
         }
     }
 }
@@ -65,8 +73,50 @@ impl UnifyKey for Key {
     }
 
     fn tag() -> &'static str {
+        // Doesn't really matter.
         "Key"
     }
+}
+
+#[derive(Clone, Debug, Copy)]
+struct Param {
+    constrained_by: Option<AbstractType>,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum ParamType {
+    ParamId(usize),
+    Abstract(AbstractType),
+}
+
+#[derive(Clone, Debug)]
+enum Expression {
+    /// Conditional expression.
+    /// Requirement: `cond` more concrete than bool, `cons` and `alt` compatible.
+    /// Returns: meet of `cons` and `alt`.
+    Conditional {
+        cond: Box<Expression>,
+        cons: Box<Expression>,
+        alt: Box<Expression>,
+    },
+    /// Polymorphic function f<T_1: C_1, ..., T_n: C_n>(p_1: C_{n+1}, ..., p_m: C_{n+m}) -> T
+    /// Arguments:
+    ///     `name` for illustration,
+    ///     `param_constraints`: vector of parameters and their constraints if any, in this example vec![C_1, ..., C_n].
+    ///     `arg_types`: vector of type constraints on the arguments, in this example vec![C_{n+1}, ..., C_{n+m}]. May refer to parameters.
+    ///     `args`: vector containing all argument expressions.
+    ///     `returns`: return type. May refer to a parameter.
+    /// Requirement: each argument needs to comply with its constraint. If several arguments refer to the same parametric type, they need to be compliant.
+    /// Returns: `returns`.
+    PolyFn {
+        name: &'static str,
+        param_constraints: Vec<Option<AbstractType>>,
+        args: Vec<(ParamType, Expression)>,
+        returns: ParamType,
+    },
+    ConstInt(i128),
+    ConstBool(bool),
+    ConstFixed(i64, u64),
 }
 
 impl UnifyValue for AbstractType {
@@ -75,11 +125,16 @@ impl UnifyValue for AbstractType {
     fn unify_values(left: &Self, right: &Self) -> Result<Self, <AbstractType as UnifyValue>::Error> {
         use crate::tests::AbstractType::*;
         match (left, right) {
+            (Any, other) | (other, Any) => Ok(other.clone()),
             (Integer(l), Integer(r)) => Ok(Integer(max(*r, *l))),
             (Fixed(li, lf), Fixed(ri, rf)) => Ok(Fixed(max(*li, *ri), max(*lf, *rf))),
             (Fixed(i, f), Integer(u)) | (Integer(u), Fixed(i, f)) if *f == 0 => Ok(Integer(max(*i, *u))),
             (Fixed(i, f), Integer(u)) | (Integer(u), Fixed(i, f)) => Ok(Fixed(max(*i, *u), *f)),
-            (Any, other) | (other, Any) => Ok(other.clone()),
+            (Bool, Bool) => Ok(Bool),
+            (Bool, _) | (_, Bool) => Err(()),
+            (Numeric, Integer(w)) | (Integer(w), Numeric) => Ok(Integer(*w)),
+            (Numeric, Fixed(i, f)) | (Fixed(i, f), Numeric) => Ok(Fixed(*i, *f)),
+            (Numeric, Numeric) => Ok(Numeric),
             (Error, _) | (_, Error) => Err(()),
         }
     }
@@ -98,22 +153,148 @@ fn bound_by_concrete_transitive() {
     let mut tc: TypeChecker<Key> = TypeChecker::new();
     let first = tc.new_key();
     let second = tc.new_key();
-    tc.impose(second.bound_by_concrete(ConcreteType::Int128));
-    tc.impose(first.more_concrete_than(second));
+    assert!(tc.impose(second.bound_by_concrete(ConcreteType::Int128)).is_ok());
+    assert!(tc.impose(first.more_concrete_than(second)).is_ok());
     assert_eq!(tc.get_type(first), tc.get_type(second));
 }
 
+fn build_type_error() -> Expression {
+    use Expression::*;
+    // Build expression `4.3 + false`
+    let const_false = ConstBool(false);
+    let const43 = ConstFixed(4, 3);
+    PolyFn {
+        name: "addition", // Signature: +<T: Numeric>(T, T) -> T
+        param_constraints: vec![Some(AbstractType::Numeric)],
+        args: vec![(ParamType::ParamId(0), const43), (ParamType::ParamId(0), const_false)],
+        returns: ParamType::ParamId(0),
+    }
+}
+
+fn create_addition(lhs: Expression, rhs: Expression) -> Expression {
+    Expression::PolyFn {
+        name: "addition", // Signature: +<T: Numeric>(T, T) -> T
+        param_constraints: vec![Some(AbstractType::Numeric)],
+        args: vec![(ParamType::ParamId(0), lhs), (ParamType::ParamId(0), rhs)],
+        returns: ParamType::ParamId(0),
+    }
+}
+
+fn build_complex_expression_type_checks() -> Expression {
+    use Expression::*;
+    // Build expression `if true then 2.7^3 + 4.3 else 3`
+    let const27 = ConstFixed(2, 7);
+    let const3 = ConstInt(3);
+    let const43 = ConstFixed(4, 3);
+    let const_true = ConstBool(true);
+    let exponentiation = PolyFn {
+        name: "exponentiation", // Signature: +<T: Numeric>(T, Int<1>) -> T
+        param_constraints: vec![Some(AbstractType::Numeric)],
+        args: vec![(ParamType::ParamId(0), const27), (ParamType::Abstract(AbstractType::Integer(1)), const3.clone())],
+        returns: ParamType::ParamId(0),
+    };
+    let addition = create_addition(exponentiation, const43);
+    Conditional { cond: Box::new(const_true), cons: Box::new(addition), alt: Box::new(const3) }
+}
+
+/// This function traverses the expression tree.
+/// It creates keys on the fly.  This is not possible for many kinds of type systems, in which case the functions
+/// requires a context with a mapping of e.g. Variable -> Key.  The context can be built during a first pass over the
+/// tree.
+fn tc_expr(
+    tc: &mut TypeChecker<Key>,
+    expr: &Expression,
+) -> Result<TypeCheckKey<Key>, <AbstractType as UnifyValue>::Error> {
+    use Expression::*;
+    let key_result = tc.new_key(); // will be returned
+    match expr {
+        ConstInt(c) => {
+            let width = (128 - c.leading_zeros()).try_into().unwrap();
+            tc.impose(key_result.bound_by_abstract(AbstractType::Integer(width)))?;
+        }
+        ConstFixed(i, f) => {
+            let int_width = (64 - i.leading_zeros()).try_into().unwrap();
+            let frac_width = (64 - f.leading_zeros()).try_into().unwrap();
+            tc.impose(key_result.bound_by_abstract(AbstractType::Fixed(int_width, frac_width)))?;
+        }
+        ConstBool(_) => tc.impose(key_result.bound_by_concrete(ConcreteType::Bool))?,
+        Conditional { cond, cons, alt } => {
+            let key_cond = tc_expr(tc, cond)?;
+            let key_cons = tc_expr(tc, cons)?;
+            let key_alt = tc_expr(tc, alt)?;
+            tc.impose(key_cond.bound_by_abstract(AbstractType::Bool))?;
+            // Two things to note regarding the next operation:
+            // The meet operation can fail.  Refer to `TypeChecker::check_conflicting(&mut self, TypeCheckerKey)` for
+            // detecting this case.  Moreover, if it fails, the conflicting type is persisted in the type table and
+            // propagated throughout the remaining type check.  If this is undesired due to the existence of an
+            // alternative type rule, refer to the snapshotting mechanism.
+            tc.impose(key_result.is_the_meet_of(&[key_cons, key_alt]))?;
+        }
+        PolyFn { name: _, param_constraints, args, returns } => {
+            // Note: The following line cannot be replaced by `vec![param_constraints.len(); tc.new_key()]` as this
+            // would copy the keys rather than creating new ones.
+            let params: Vec<(Option<AbstractType>, TypeCheckKey<Key>)> =
+                param_constraints.iter().map(|p| (*p, tc.new_key())).collect();
+            dbg!(&params);
+            for (arg_ty, arg_expr) in args {
+                let arg_key = tc_expr(tc, arg_expr)?;
+                match dbg!(arg_ty) {
+                    ParamType::ParamId(id) => {
+                        let (p_constr, p_key) = dbg!(params[*id]);
+                        // We need to enforce that the parameter is more concrete than the passed argument and that the
+                        // passed argument satisfies the constraints imposed on the parametric type.
+                        tc.impose(p_key.more_concrete_than(arg_key))?;
+                        if let Some(c) = p_constr {
+                            tc.impose(dbg!(arg_key).bound_by_abstract(c))?;
+                            println!("Argument is of abstract type {:?}.", tc.get_type(arg_key));
+                        }
+                    }
+                    ParamType::Abstract(at) => tc.impose(arg_key.bound_by_abstract(*at))?,
+                };
+            }
+            match returns {
+                ParamType::Abstract(at) => tc.impose(key_result.bound_by_abstract(*at))?,
+                ParamType::ParamId(id) => {
+                    let (constr, key) = params[*id];
+                    if let Some(c) = constr {
+                        tc.impose(key_result.bound_by_abstract(c))?;
+                    }
+                    tc.impose(key_result.more_concrete_than(key))?;
+                }
+            }
+        }
+    }
+    Ok(key_result)
+}
+
 #[test]
-fn bound_then_refine() {
+fn complex_type_check() {
+    let expr = build_complex_expression_type_checks();
     let mut tc: TypeChecker<Key> = TypeChecker::new();
-    let a = tc.new_key();
-    let b = tc.new_key();
-    let first_bound = AbstractType::Integer(3);
-    let second_bound = AbstractType::Integer(8);
-    tc.impose(b.bound_by_abstract(first_bound));
-    tc.impose(a.more_concrete_than(b));
-    tc.impose(b.bound_by_abstract(second_bound));
-    assert_ne!(tc.get_type(a), tc.get_type(b));
-    assert_eq!(tc.get_type(a), first_bound);
-    assert_eq!(tc.get_type(b), second_bound);
+    let res = tc_expr(&mut tc, &expr);
+    match res {
+        Ok(key) => {
+            let res_type = tc.get_type(key);
+            // Expression `if true then 2.7^3 + 4.3 else 3` should yield type Fixed(3, 3) because the addition requires a
+            // Fixed(2,3) and a Fixed(3,3), which results in a Fixed(3, 3).
+            assert_eq!(res_type, AbstractType::Fixed(3, 3));
+        }
+        Err(_) => panic!("Unexpected type error!"),
+    }
+}
+
+#[test]
+fn failing_type_check() {
+    let expr = build_type_error();
+    let mut tc: TypeChecker<Key> = TypeChecker::new();
+    // Expression `4.3 + false` should yield an error.
+    let res = tc_expr(&mut tc, &expr);
+    match res {
+        Ok(key) => {
+            let res_type = tc.get_type(key);
+            panic!("Unexpectedly got result type {:?}", res_type);
+        }
+        Err(_) => {}
+    }
+    println!("{:?}", tc.get_state());
 }
