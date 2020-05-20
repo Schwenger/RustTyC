60ee0073941f031eb84128894896c04aefc829aa
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2020-05-20 09:47:58 +0200

    Remove outdated docs.

--- src/lib.rs
+++ src/lib.rs
@@ -18,51 +18,7 @@
 //! Lastly, `Variable` represents a re-usable variable during the type checking procedure and needs to implement
 //! `rusttyc::TcVar`.
 //! ```
-//! use rusttyc::{ TcKey, TypeChecker, EnaValue, EnaKey };
-//! use ena::unify::UnifyValue;
-//! #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-//! struct Key(u32);
-//! #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-//! struct Variable(u32);
-//! impl rusttyc::TcVar for Variable {}
-//! #[derive(Debug, PartialEq, Eq, Clone)]
-//! enum AbstractType {
-//!     Variant1,
-//!     Unconstrained,
-//!     /* ... */
-//! }
-//! impl rusttyc::Abstract for AbstractType {
-//!     fn unconstrained() -> Self {
-//!         Self::Unconstrained
-//!     }
-//! }
-//! impl EnaValue for AbstractType {
-//!     type Error = ();
-//!
-//!     fn unify_values(value1: &Self, value2: &Self) -> Result<Self, Self::Error> {
-//!         /* Do something meaningful. */
-//!         return Ok(value1.clone());
-//!     }
-//! }
-//! impl EnaKey for Key {
-//!     type Value = AbstractType;
-//!
-//!     fn index(&self) -> u32 { self.0 }
-//!
-//!     fn from_index(u: u32) -> Self { Key(u) }
-//!
-//!     fn tag() -> &'static str { "key" }
-//! }
-//!
-//! let mut tc: TypeChecker<Key, Variable> = TypeChecker::new();
-//!
-//! let first = tc.new_term_key();
-//! let second = tc.new_term_key();
-//!
-//! assert!(tc.impose(second.bound_by_abstract(AbstractType::Variant1)).is_ok());
-//! assert!(tc.impose(first.more_concrete_than(second)).is_ok());
-//!
-//! assert_eq!(tc.get_type(first), tc.get_type(second));
+//!  todo!("Insert Example!");
 //! ```
 //!
 //! For a full example, refer to the example directory.
