dd3ce103502b0f8c9d3b39dfd30f5eb3d66983c4
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2020-08-25 09:54:39 +0200

    Move gh page to separate branch.

--- public/doc/implementors/core/hash/trait.Hash.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rusttyc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rusttyc/struct.TcKey.html\" title=\"struct rusttyc::TcKey\">TcKey</a>","synthetic":false,"types":["rusttyc::keys::TcKey"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- public/doc/implementors/core/marker/trait.StructuralEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rusttyc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"rusttyc/struct.TcKey.html\" title=\"struct rusttyc::TcKey\">TcKey</a>","synthetic":false,"types":["rusttyc::keys::TcKey"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
